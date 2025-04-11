f185f1b7f06928afd3fb53d4324bc7816ac17ee0
Aidan <aidandepaul@gmail.com>
2025-04-11 18:54:02 -0400

    Add variable to create div with class "row"
    Create div with class "row" to store divs with class "square"

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector("#grid");
+const row = document.createElement("div");
+row.classname = "row";
 const square = document.createElement("div");
 square.className = "square";
-
