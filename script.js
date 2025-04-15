3a8e7113787c360a98965844132628d4fd292075
Aidan <aidandepaul@gmail.com>
2025-04-14 23:15:14 -0400

    Add makeGrid function to create makeGrid
    Use 2 for loops:
    - 1 to append 16 rows to #grid
    - 1 to append 16 squares to each row

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ row.classname = "row";
 const square = document.createElement("div");
 square.className = "square";
 
+/* Ideas for making a 16x16 grid...
+2 "for" loops: 1 to create 16 rows and one to put 16 squares in the row
+*/
+
+const makeGrid = () => {
+    for (i = 0; i < 16; i++) {
+        grid.AppendChild(row);
+    };
+    for (i = 0; i < 16; i++) {
+        row.AppendChild(square);
+    }
+};
