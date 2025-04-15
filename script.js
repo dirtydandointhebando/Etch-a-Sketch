e02bd5b9cf1df8b57f671b89d1fbc96be7e26fde
Aidan <aidandepaul@gmail.com>
2025-04-14 23:55:24 -0400

    Create row variable to querySelectorAll(".row");

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const newRow = document.createElement("div");
 newRow.classname = "row";
 const square = document.createElement("div");
 square.className = "square";
+const row = document.querySelectorAll(".row");
 
 /* Ideas for making a 16x16 grid...
 2 "for" loops: 1 to create 16 rows and one to put 16 squares in the row
