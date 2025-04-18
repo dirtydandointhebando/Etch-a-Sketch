3887fcaee15911576e9d3a7ff27d56d5724a8ac0
Aidan <aidandepaul@gmail.com>
2025-04-18 10:38:20 -0400

    Create first full version of makeGrid function

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const grid = document.querySelector("#grid");
 
 const makeGrid = () => {
     for (let i = 0; i < 16; i++) {
+        const newRow = document.createElement("div");
+        newRow.className = "row";
 
+        for (let r = 0; r < 16; r++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            newRow.appendChild(square);
+        }
+        grid.appendChild(newRow);
     }
 };
+
+makeGrid();
