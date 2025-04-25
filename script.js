cf43b29a956d506b804438506d30e2c9af2aeb4f
Aidan <aidandepaul@gmail.com>
2025-04-24 22:54:56 -0400

    Change while loop in btn function to do while loop

--- script.js
+++ script.js
@@ -27,11 +27,10 @@ document.querySelectorAll(".square").forEach(square => {
 
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-    let dimensions = prompt("How many squares do you want per row? Please enter a number between 1-100.");
-    while (!Number.isInteger(Math.floor(dimensions)) || (dimensions > 100)) {
-        let dimensions = prompt("Error. Please enter a valid number between 1-100.");
-        if (Number.isInteger(dimensions) && (dimensions < 100)) break;
-    };
+    let dimensions;
+    do {
+        let dimensions = prompt("How many squares do you want per row? Please enter a number between 1-100.");
+    } while (!Number.isInteger(Math.floor(dimensions)) || (dimensions > 100));
     // make sure max value allowed is 100
     // add error message that makes user re-enter input if not an int or if it is > 100
     // create a square grid with the specified height and width as the input
